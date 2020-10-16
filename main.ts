2e8e1bea0f1f533b5f375fab59e7d968ae54341b
Vitaliy Kotyak <v.kotyak@gmail.com>
2020-10-16 12:41:18 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let oldy = 0
+let oldx = 0
 let N = 0
 let x = 0
 let y = 0
@@ -6,6 +8,8 @@ let d = 0
 let k = 0
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
+    oldy = 0
+    oldx = 0
     N = 5
     x = -1
     y = 0
@@ -19,6 +23,11 @@ input.onButtonPressed(Button.A, function () {
             r += 1
             led.plot(y, x)
             basic.pause(100)
+            if (!(x == 0 && y == 0)) {
+                led.unplot(oldy, oldx)
+            }
+            oldy = y
+            oldx = x
             k += 1
         }
         d += 1
